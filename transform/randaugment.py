d3234920677a58e68dfee7f53c5e4e47be6fe3d6
Aman Shrivastava <aman.srivastava999@gmail.com>
2022-06-16 15:12:30 -0700

    Added Timestep noise sche

--- transform/randaugment.py
+++ transform/randaugment.py
@@ -5,6 +5,29 @@ import torch
 IMAGENET_MEAN = (0.48145466, 0.4578275, 0.40821073)
 IMAGENET_STD = (0.26862954, 0.26130258, 0.27577711)
 
+def extract(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+
+def cosine_beta_schedule(timesteps, s = 0.008, thres = 0.999):
+    """
+    cosine schedule
+    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
+    """
+    steps = timesteps + 1
+    x = torch.linspace(0, timesteps, steps, dtype = torch.float64)
+    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * torch.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0, thres)
+
+def linear_beta_schedule(timesteps):
+    scale = 1000 / timesteps
+    beta_start = scale * 0.0001
+    beta_end = scale * 0.02
+    return torch.linspace(beta_start, beta_end, timesteps, dtype = torch.float64)
+
 ## Tensor-transforms
 # Gaussin noise transforms
 class RandGaussianNoise(object):
@@ -21,6 +44,39 @@ class RandGaussianNoise(object):
     def __repr__(self):
         return self.__class__.__name__ + "(max_scale={0})".format(self.max_scale)
 
+
+class TimestepNoise(object):
+    def __init__(self, timesteps=1000, noise_schedule="cosine"):
+        if noise_schedule == "cosine":
+            self.betas = cosine_beta_schedule(timesteps).to(torch.float32)
+        elif noise_schedule == "linear":
+            self.betas = linear_beta_schedule(timesteps).to(torch.float32)
+        else:
+            raise NotImplementedError()
+
+        self.alphas = 1. - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, axis = 0)
+
+        timesteps, = self.betas.shape
+        self.num_timesteps = int(timesteps)
+
+        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod)
+
+    def __call__(self, tensor):
+        noise = torch.randn_like(tensor)
+        t = torch.randint(0, self.num_timesteps, (tensor.shape[0],),)
+
+        noised = (
+            extract(self.sqrt_alphas_cumprod, t, tensor.shape) * tensor +
+            extract(self.sqrt_one_minus_alphas_cumprod, t, tensor.shape) * noise
+        )
+
+        return noised
+
+    def __repr__(self):
+        return self.__class__.__name__
+        
 
 ## aug functions
 def identity_func(img):
